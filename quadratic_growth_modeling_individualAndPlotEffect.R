db9fcd19d717d182bffe56566c23b39d4a608080
Mary Lofton <melofton@vt.edu>
2025-02-24 11:39:43 -0500

    drafting linear model

--- quadratic_growth_modeling_individualAndPlotEffect.R
+++ quadratic_growth_modeling_individualAndPlotEffect.R
@@ -34,7 +34,7 @@ run_model <- function(k, df, sim){
     group_by(tree_ID) |> 
     tidyr::fill(subp_BA_GT_m1, .direction = "down") |> 
     dplyr::filter(!is.na(subp_BA_GT_m1) & !is.na(Dep_N)) |>
-    ungroup()
+    ungroup() 
   
   trees_index <- focal_data |> 
     dplyr::filter(common_name == tree_species) |> 
@@ -53,8 +53,7 @@ run_model <- function(k, df, sim){
     select(AG_carbon_pYear, AG_carbon_m1, AG_carbon_m2, tree_ID, plot_ID, dt, Dep_N, subp_BA_GT_m1, MAT, MAP, Dep_S) |>
     dplyr::filter(tree_ID %in% live_tree_ids) |>
     left_join(plots, by = join_by(plot_ID)) |> 
-    left_join(trees_index, by = join_by(tree_ID)) %>%
-    slice(c(1:100))
+    left_join(trees_index, by = join_by(tree_ID)) 
   
   mean_annual_avg_growth <- df1$AG_carbon_pYear
   start_measures <- df1$AG_carbon_m1
@@ -96,8 +95,8 @@ run_model <- function(k, df, sim){
   procErr ~ dunif(0.0001,10)
   p2 ~ dunif(0,2) 
   p3 ~ dunif(0,100) #Sample in log space
-  p4 ~ dunif(-1000, 1000)#; NEED TO VECTORIZE THIS TO N=3 FOR DIFFERENT STATUSES
-  p5 ~ dunif(0.3, 20) # ; ALSO VECTORIZE THIS
+  p4 ~ dnorm(0,0.001)#; NEED TO VECTORIZE THIS TO N=3 FOR DIFFERENT STATUSES
+  p5 ~ dnorm(0,0.001) # ; ALSO VECTORIZE THIS
   #lp4 ~ dunif(log(min_ndep), log(max_ndep*2))
   #p5inv2 ~ dexp(0.001)  
   #p6 ~ dunif(min_temp*0.5, max_temp*2)
@@ -120,10 +119,9 @@ run_model <- function(k, df, sim){
   for(t in 1:n_measures){
     tree_agb_latent[t] ~ dnorm(tree_agb_obs[t], obsErr)
     
-    tree_growth_mean[t] <-  (tree_effect[tree_index[t]] * tree_agb_latent[t] ^ p2) 
+    tree_growth_mean[t] <-  ((tree_effect[tree_index[t]] + ndep[t]*p4 + ndep[t]^2 * p5) * tree_agb_latent[t] ^ p2) 
         * exp(-ba_gt[t]*p3)  
-        * exp(-0.5 * (log(ndep[t] / p4)/ p5) ^ 2)  
-        
+
     tree_growth_latent[t] ~ dnorm(tree_growth_mean[t], procErr)
     tree_growth_obs[t] ~ dnorm(tree_growth_latent[t], obsErr)
     
@@ -137,26 +135,26 @@ run_model <- function(k, df, sim){
                      "procErr" = 0.001,
                      "p2" = 0.6,
                      "p3" = 1,
-                     "p4" = max(c(ndep), na.rm = TRUE), 
-                     "p5" = 1.3),
+                     "p4" = 0.5, 
+                     "p5" = 0.5),
                 list("global_tree_effect" = 1,
                      "tau_global" = 1,
                      "tau_plot" = 1,
                      "procErr" = 0.001,
                      "p2" = 0.4,
                      "p3" = 1,
-                     "p4" = 2 * min(c(ndep), na.rm = TRUE), 
-                     "p5" = 2),
+                     "p4" = 0, 
+                     "p5" = 0),
                 list("global_tree_effect" = 1,
                      "tau_global" = 1,
                      "tau_plot" = 1,
                      "procErr" = 0.001,
                      "p2" = 0.4,
                      "p3" = 1,
-                     "p4" = 0.5 * max(c(ndep), na.rm = TRUE), 
-                     "p5" = 5))
+                     "p4" = -0.5, 
+                     "p5" = -0.5))
   
-  ssFit    <- jags.model(data=ssData, file=paste0(sim,"-growthModel-",k,".txt"), n.chains = 3, inits = inits, n.adapt = 5000)
+  ssFit    <- jags.model(data=ssData, file=paste0(sim,"-growthModel-",tree_species,".txt"), n.chains = 3, inits = inits, n.adapt = 5000)
   parNames <- c("tree_effect", 
                 "p2", 
                 "p3", 
@@ -180,7 +178,7 @@ run_model <- function(k, df, sim){
                        `plot_effect[20]`) 
   
   
-  arrow::write_parquet(mcmc, sink = paste0(sim, "-",k,"-",tree_species, "-mcmc.parquet"))
+  arrow::write_parquet(mcmc, sink = paste0(sim, "-",tree_species, "-mcmc.parquet"))
   
   p <- mcmc |> 
     rename(chain = .chain, iteration = .iteration, draw = .draw) |> 
@@ -190,7 +188,7 @@ run_model <- function(k, df, sim){
     geom_line() + 
     facet_wrap(~par, scales = "free")
   
-  ggsave(filename = paste0(sim, "-",k,"-",tree_species,"-mcmc.pdf"), plot = p, device = "pdf")
+  ggsave(filename = paste0(sim, "-",tree_species,"-mcmc.pdf"), plot = p, device = "pdf")
   
   n_samples <- 1000
   samples <- sample(nrow(mcmc), n_samples, replace = FALSE)
@@ -205,42 +203,6 @@ run_model <- function(k, df, sim){
   mcmc_burned <- mcmc |> 
     dplyr::filter(.iteration > 1000)
   
-  for(i in 1:length(samples)){
-    
-    curr_ndep <- tibble(sample = i,
-                        variable = "ndep (kg N/m2/yr)",
-                        value = ndep_pred,
-                        pred = exp(-0.5 * (log(ndep_pred / mcmc_burned$p4[samples[i]])/ mcmc_burned$p5[samples[i]]) ^ 2),
-                        type = "posterier")
-    
-    pred_post <- bind_rows(pred_post, curr_ndep)
-  }
-  
-  pred_prior <- NULL
-  
-  for(i in 1:1000){
-    p4 <- runif(1, min_ndep*0.5, max_ndep*2)
-    p5 <- runif(1, 0.3, 20) 
-    
-    curr_ndep <- tibble(sample = i,
-                        variable = "ndep (kg N/m2/yr)",
-                        value = ndep_pred,
-                        pred = exp(-0.5 * (log(ndep_pred / p4)/ p5) ^ 2),
-                        type = "prior")
-    
-    pred_prior <- bind_rows(pred_prior, curr_ndep)
-    
-  }
-  
-  p <- bind_rows(pred_prior, pred_post) |> 
-    mutate(id = paste0(sample,type)) |> 
-    ggplot(aes(x = value, y = pred, group = id, color = type)) +
-    geom_line() +
-    facet_wrap(~variable, scales = "free_x") +
-    theme_bw()
-  
-  ggsave(filename = paste0(sim, "-", k,"-",tree_species,"-prior-post.pdf"), plot = p, device = "pdf")
-  
   p <- mcmc_burned |> 
     rename(chain = .chain,
            iteration = .iteration) |> 
@@ -249,7 +211,7 @@ run_model <- function(k, df, sim){
     mcmc_pairs()
   
   
-  ggsave(filename = paste0(sim, "-", k,"-",tree_species,"-pairs.pdf"), plot = p, device = "pdf", height = 12, width = 12)
+  ggsave(filename = paste0(sim, "-", tree_species,"-pairs.pdf"), plot = p, device = "pdf", height = 12, width = 12)
   
 
 }
--- run_ss_model.R
+++ run_ss_model.R
@@ -11,11 +11,11 @@ library(bayesplot)
 library(furrr)
 
 df <- read_csv("./McDonnell_etal_InPrep_TreeData_2024_10_11.csv", show_col_types = FALSE) %>%
-  filter(common_name %in% c("red spruce")) 
+ filter(common_name %in% c("ponderosa pine")) 
 
 total_species <- length(unique(df$common_name))
 
-sim <- "checking-merge"
+sim <- "ponderosa-pine"
 
 source("./growth_modeling_individualAndPlotEffect.R")
 
