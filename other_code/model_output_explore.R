2ab421005bdccf406e096f6cf3069c80b35ec5e0
Mary Elizabeth Lofton <melofton@tc005.cm.cluster>
2025-03-31 12:23:12 -0400

    tweaks to plotting code

--- other_code/model_output_explore.R
+++ other_code/model_output_explore.R
@@ -70,20 +70,25 @@ delta_growth_vs_delta_Ndep(data = "./data/McDonnell_etal_InPrep_TreeData_2024_10
                model_output_folder = "./experiments/delta_Ndep",
                experiment_name = "delta_Ndep_only")
 
-# checking param distributions with Ndep only vs all env variables
+# checking param distributions with Ndep only vs all env variables vs
+# separating N into different species
 
 # list files
-out1 <- list.files("./experiments/delta_Ndep_only",pattern = ".parquet",
+out1 <- list.files("./experiments/new_delta_Ndep_only",pattern = ".parquet",
                   full.names = TRUE)
 out2 <- list.files("./experiments/delta_env",pattern = ".parquet",
                    full.names = TRUE)
-out <- c(out1,out2)
+out3 <- list.files("./experiments/N_species",pattern = ".parquet",
+                   full.names = TRUE)
+out <- c(out1,out2, out3)
 
 for(i in 1:length(out)){
   
-  model_name = str_split(out[i], pattern = "-")[[1]][2]
+  spp_id = str_split(out[i], pattern = "-")[[1]][2]
+  model_id = str_split(out[i], pattern = "/")[[1]][3]
   temp <- read_parquet(file = out[i]) %>%
-    mutate(model_id = model_name)
+    mutate(spp_id = spp_id,
+           model_id = model_id)
   
   if(i == 1){
     final <- temp
@@ -93,8 +98,42 @@ for(i in 1:length(out)){
   
 }
 
-final <- final %>%
-  mutate(model_id = ifelse(model_id == "yellow","yellow poplar",model_id))
+final1 <- final %>%
+  mutate(spp_id = ifelse(spp_id == "yellow","yellow poplar",spp_id)) %>%
+  select(model_id, spp_id, p2, p3, p4, p5, p6, p7, p8) %>%
+  pivot_longer(p2:p8, names_to = "parameter_name", values_to = "parameter_value") %>%
+  mutate(parameter_name = ifelse(model_id %in% c("new_delta_Ndep_only") & parameter_name == "p5","delta_Ndep_only",
+                                 ifelse(model_id == "N_species" & parameter_name == "p4","delta_Ndep_oxi",
+                                        ifelse(model_id == "N_species" & parameter_name == "p5","delta_Ndep_red",
+                                               ifelse(model_id == "delta_env" & parameter_name == "p5","delta_Ndep_env",parameter_name))))) %>%
+  filter(parameter_name %in% c("delta_Ndep_only","delta_Ndep_oxi","delta_Ndep_red", "delta_Ndep_env"))
+
+ggplot(data = final1)+
+  geom_density(aes(x = parameter_value, group = parameter_name, 
+                   color = parameter_name, fill = parameter_name),
+               alpha = 0.5)+
+  facet_wrap(facets = vars(spp_id), scales = "free")+
+  theme_bw()
+
+final2 <- final1 %>%
+  filter(parameter_name %in% c("delta_Ndep_only","delta_Ndep_env"))
+
+ggplot(data = final2)+
+  geom_density(aes(x = parameter_value, group = parameter_name, 
+                   color = parameter_name, fill = parameter_name),
+               alpha = 0.5)+
+  facet_wrap(facets = vars(spp_id), scales = "free")+
+  theme_bw()
+
+final3 <- final1 %>%
+  filter(parameter_name %in% c("delta_Ndep_oxi","delta_Ndep_red","delta_Ndep_env"))
+
+ggplot(data = final3)+
+  geom_density(aes(x = parameter_value, group = parameter_name, 
+                   color = parameter_name, fill = parameter_name),
+               alpha = 0.5)+
+  facet_wrap(facets = vars(spp_id), scales = "free")+
+  theme_bw()
 
 ggplot(data = final)+
   geom_density(aes(x = p4, group = model_id, color = model_id, fill = model_id),
